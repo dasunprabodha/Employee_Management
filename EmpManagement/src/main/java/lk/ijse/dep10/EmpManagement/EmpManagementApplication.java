34bc0d9447bb5bcad167e254c1cfb286aa33ca24
dasun <amarasinghe.dasun61@gmail.com>
2024-04-01 15:42:00 +0530

    Swagger doc.

--- EmpManagement/src/main/java/lk/ijse/dep10/EmpManagement/EmpManagementApplication.java
+++ EmpManagement/src/main/java/lk/ijse/dep10/EmpManagement/EmpManagementApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.dep10.EmpManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EmpManagementApplication {
 
 	public static void main(String[] args) {
--- EmpManagement/src/test/java/lk/ijse/dep10/EmpManagement/EmpManagementApplicationTests.java
+++ EmpManagement/src/test/java/lk/ijse/dep10/EmpManagement/EmpManagementApplicationTests.java
@@ -2,8 +2,10 @@ package lk.ijse.dep10.EmpManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
+
 class EmpManagementApplicationTests {
 
 	@Test
